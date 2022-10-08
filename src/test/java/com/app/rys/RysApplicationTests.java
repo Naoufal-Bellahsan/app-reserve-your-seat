6db21b7a87f5445b8b98341d903577a429d477fe
unknown <naufal.zizu5@gmail.com>
2022-10-08 12:57:04 +0200

    Inicializar proyecto

---
+++ src/test/java/com/app/rys/RysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.rys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
