27d37838eeac454dce5a88b25160bbb60804200d
command <command>
2022-10-15 22:05:09 +0200

    Entidad reservada creada

---
+++ src/main/java/com/app/rys/enums/BookingState.java
@@ -0,0 +1,13 @@
+package com.app.rys.enums;
+
+public enum BookingState {
+	ANULADA, PENDIENTE, CONFIRMADA;
+	
+	private String BookingState;
+	
+	public String getState() {
+		return BookingState;
+	}
+		
+
+}
