214ea61223de0a743c25b613af5d7107d0b6370a
naufal <naufal.zizu5@gmail.com>
2022-12-05 19:17:00 +0100

    RYS-17 inicializar angular

--- src/main/java/com/app/rys/controller/AppController.java
+++ src/main/java/com/app/rys/controller/AppController.java
@@ -2,6 +2,8 @@ package com.app.rys.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
